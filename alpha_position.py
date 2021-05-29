da12f6c140d64575b73f13e844dbb08e773de0ac
John Doe <johndoe@example.com>
2021-05-29 18:16:14 -0400

    alpha position

---
+++ alpha_position.py
@@ -0,0 +1,17 @@
+
+
+def alphabet_position(text):
+  sent = ""
+  for a in text:
+    if a.isalpha():
+      sent = sent + " " + str(ord(a.lower()) - 96)
+    else:
+      continue
+  return sent
+
+
+print(alphabet_position("The sunset sets at twelve o' clock."))
+
+## better version
+def alphabet_position(text):
+    return ' '.join(str(ord(c) - 96) for c in text.lower() if c.isalpha())
