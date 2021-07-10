1bda3df7aedd40a61e14a69a89364a4308db3b52
nosleep 77 <nosleep77@example.com>
2021-07-09 22:25:21 -0400

    oop examples

---
+++ oop/classtest.py
@@ -0,0 +1,18 @@
+class Time:
+    # previous method definitions here...
+
+    def after(self, other):
+        if self.hours > other.hours:
+            return True
+        if self.hours < other.hours:
+            return False
+
+
+time1 = Time()
+time2 = Time()
+
+time1.hours = 3
+time2.hours = 2
+
+print(time1.after(time2))
+
