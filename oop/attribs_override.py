29df99945a2248bc77768acfd4e4ddd96b7f319b
nosleep 77 <nosleep77@example.com>
2021-07-11 21:03:02 -0400

    update to attrib

--- oop/attribs_override.py
+++ oop/attribs_override.py
@@ -1,5 +1,7 @@
 class Robot:
 
+  model = "T1000"
+
   def __init__(self, name="Tom", color="green", weight=30) -> None:
     self.name = name
     self.color = color
@@ -8,6 +10,19 @@ class Robot:
   def intro(self):
     print(f"my name is {self.name}. color is {self.color}. weight is {self.weight}")
 
+  @classmethod
+  def get_model(cls):
+    return cls.model
+
+  @staticmethod
+  def ranrobot():
+    return "a random robot"
 
 r1 = Robot("Jack","blue",45)
 r1.intro()
+
+Robot.model = "T2000"
+
+print(f"model is {Robot.get_model()}")
+
+print(f"model is {r1.ranrobot()}")
