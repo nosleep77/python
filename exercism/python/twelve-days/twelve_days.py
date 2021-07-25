0a3a99e022d66483e75b7dd025028d16bfcf44da
nosleep 77 <nosleep77@example.com>
2021-07-25 14:25:51 -0400

    update

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -24,3 +24,14 @@ def getlines(n,days,begin):
   return "".join([begin, "".join([days[a][1] for a in range(n,1-1,-1)])])
 
 
+## Feedback squeakyboots Mentor - exercism
+# Looking good! Certainly approved =D
+# If you're up for another iteration consider switching to lists or tuples. 
+# While dictionaries are great structures for unordered look ups they 
+# offer no advantage over a list or tuple for nth item look ups while taking up more memory.
+# Consider a list of lists (or tuples) instead. Index 0 could be 
+# padded with the text from line 21 so that the ordinals line up with their indexes naturally.
+# Also, constants like days are usually kept at the module level and 
+# use all caps as a naming convention per PEP 8.
+# Overall a very nice solution! It's good to see you're comfortable 
+# with breaking the problem out into smaller pieces via helper functions.
