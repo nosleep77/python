2f856c822993d90b854e077079c15da4603c9ac0
nosleep 77 <nosleep77@example.com>
2022-03-20 15:39:18 -0400

    update

--- fib.py
+++ fib.py
@@ -11,18 +11,14 @@ def get_fib(n):
 '''
 
 def get_fib(n):
-
   a = 0
   b = 1
-
   for i in range(n):
     a, b = b, a+b
-    
   yield b
 
 
 n = 20
-
 for i in range(n+1):
   print(f"{i}: {list(get_fib(i))}")
 
